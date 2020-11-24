ca1efbb5348427ba076299b12b3c0e2ea8e196b4
Tert-butyllithium <lihaonan1105@gmail.com>
2020-11-24 21:59:37 +0800

    add comments

--- etm.h
+++ etm.h
@@ -373,7 +373,7 @@ static void etm4_disable_hw(void *info)
 
     CS_LOCK(drvdata->base);
 
-    dev_dbg(drvdata->dev, "cpu: %d disable smp call done\n", drvdata->cpu);
+    printk(KERN_INFO "[" DRVR_NAME "]", "cpu: %d disable smp call done\n", drvdata->cpu);
 }
 
 static void etm4_enable_hw(void *info)
@@ -485,7 +485,9 @@ static void etm4_enable_hw(void *info)
 
     CS_LOCK(drvdata->base);
 
-    dev_dbg(drvdata->dev, "cpu: %d enable smp call done\n", drvdata->cpu);
+    printk(KERN_INFO "[" DRVR_NAME "]"
+                     "cpu: %d enable smp call done\n",
+           drvdata->cpu);
 }
 
 #endif
